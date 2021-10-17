b43e71218b37437b30b88fe8ad8dfacc4e80f1df
Adrib Wadud <adribwadud@Adribs-MacBook-Pro.local>
2021-10-16 19:09:17 -0500

    Got Google Oauth working

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  ENV['GOOGLE_OAUTH_CLIENT_ID'] = '52567173804-avbva962t1t439mqjsps3lvbikvhlmhr.apps.googleusercontent.com'
+  ENV['GOOGLE_OAUTH_CLIENT_SECRET'] = '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'
 end
