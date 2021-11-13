ada55257f7fa8b15783a4a55a64bd394265bb215
Joao Pedro Schuh <joaoschuh23@tamu.edu>
2021-11-12 21:26:07 -0600

    Unit and integration tests for members
    Added unit and integration tests for members

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,4 +2,10 @@
 
 class Member < ApplicationRecord
   validates :name, presence: true
+  validates :description, presence: true
+  validates :birthday, presence: true
+  validates :classification, presence: true
+  validates :major, presence: true
+  validates :email, presence: true
+  validates :phone, presence: true
 end
