acc91eb409430ff579b670b10c5ea36ec8dcc1ff
ChristianRossKane <christian.kane@hotmail.com>
2021-11-13 15:57:21 -0600

    add access control for gallery; has members and members tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2021_11_10_015308) do
-=======
-ActiveRecord::Schema.define(version: 2021_11_07_211801) do
->>>>>>> ada55257f7fa8b15783a4a55a64bd394265bb215
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
